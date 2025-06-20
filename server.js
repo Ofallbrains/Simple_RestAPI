b01bb7cc7830415e9a744ab2a7afe2aaa9d17977
Ofallbrains <ofallbrains@gmail.com>
2025-06-20 17:21:45 +0200

    Implemented GET method for getting a user by ID

--- server.js
+++ server.js
@@ -9,16 +9,24 @@ let nextID = 1;
 // POST /users
 app.post('/users', (req, res) => {
     const { name, email } = req.body;
-})
+    
+    if (!name || !email) {
+        return res.status(400).json({ message: 'Name and email are required!' })
+    }
+    const user = {
+        id: nextID++,
+        name,
+        email
+    }
+    users.push(user);
+    res.status(201).json(user)
+});
 
-if (!name || !email) {
-    return res.status(400).json({message: 'Name and email are required!'})
-}
-const user = {
-    id: nextID++,
-    name,
-    email
-}
-users.push(user);
-res.status(201).json(user)
+//GET /users/:id
+app.get('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ message: 'User not found' });
+  res.json(user);
+});
+
 
