c2d7b980afec4932e206d7c4b74eb7345b68f439
Ofallbrains <ofallbrains@gmail.com>
2025-06-20 17:31:04 +0200

    Built a simple Rest API

--- server.js
+++ server.js
@@ -23,10 +23,15 @@ app.post('/users', (req, res) => {
 });
 
 //GET /users/:id
-app.get('/api/users/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
   const user = users.find(u => u.id === parseInt(req.params.id));
   if (!user) return res.status(404).json({ message: 'User not found' });
   res.json(user);
 });
 
+//Start the server
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+    
+})
 
